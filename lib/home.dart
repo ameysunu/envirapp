82c2750592a93a8d753248a24f4c1c1be8a3d666
ameysunu <ameysunu00719@gmail.com>
2020-08-13 15:11:09 +0300

    scrollview added

--- lib/home.dart
+++ lib/home.dart
@@ -24,109 +24,197 @@ class _HomeState extends State<Home> {
       home: Scaffold(
         backgroundColor: Hexcolor("#0094AC"),
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            Card(
-              color: Hexcolor('#B2EC80'),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(25.0),
-                  bottom: Radius.circular(25.0),
-                ),
-              ),
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: Text(
-                        'Envirapp',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontFamily: 'OpenSans',
-                            fontSize: 30.0,
-                            color: Hexcolor('#21697A')),
-                      ),
-                    ),
+          child: Column(
+            children: <Widget>[
+              Card(
+                color: Hexcolor('#B2EC80'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25.0),
+                    bottom: Radius.circular(25.0),
                   ),
-                ],
-              ),
-            ),
-            Card(
-              color: Hexcolor('#BD309A'),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(25.0),
-                  bottom: Radius.circular(25.0),
                 ),
-              ),
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: SizedBox(
-                height: 300.0,
-                width: double.infinity,
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      child: SizedBox(
+                        width: double.infinity,
                         child: Text(
-                          'Let\'s make a change!',
+                          'Envirapp',
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                               fontFamily: 'OpenSans',
                               fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                              color: Hexcolor('#21697A')),
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
-                        child: Text(
-                          'We love our resources and so we do want to save them.',
-                          style: TextStyle(
-                              fontFamily: 'OpenSans',
-                              fontSize: 20.0,
-                              color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              Card(
+                color: Hexcolor('#BD309A'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25.0),
+                    bottom: Radius.circular(25.0),
+                  ),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: SizedBox(
+                  height: 300.0,
+                  width: double.infinity,
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+                          child: Text(
+                            'Let\'s make a change!',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontSize: 30.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                        child: Text(
-                          'Send us a mail to know more.',
-                          style: TextStyle(
-                              fontFamily: 'OpenSans',
-                              fontSize: 17.0,
-                              color: Colors.white70),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
+                          child: Text(
+                            'We love our resources and so we do want to save them.',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontSize: 20.0,
+                                color: Colors.white),
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(90, 30, 10, 0),
-                        child: ButtonBar(
-                          children: <Widget>[
-                            RaisedButton(
-                              color: Hexcolor('#0094AC'),
-                              onPressed: () {
-                                _launchURL('inquiry@unep.org');
-                              },
-                              shape: StadiumBorder(),
-                              child: Text('Mail UNEP helpline',
-                                  style: TextStyle(
-                                      fontFamily: 'OpenSans',
-                                      fontSize: 20,
-                                      color: Hexcolor('#FFFFFF'))),
-                            ),
-                          ],
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+                          child: Text(
+                            'Send us a mail to know more.',
+                            style: TextStyle(
+                                fontFamily: 'OpenSans',
+                                fontSize: 17.0,
+                                color: Colors.white70),
+                          ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(90, 30, 10, 0),
+                          child: ButtonBar(
+                            children: <Widget>[
+                              RaisedButton(
+                                color: Hexcolor('#0094AC'),
+                                onPressed: () {
+                                  _launchURL('inquiry@unep.org');
+                                },
+                                shape: StadiumBorder(),
+                                child: Text('Mail UNEP helpline',
+                                    style: TextStyle(
+                                        fontFamily: 'OpenSans',
+                                        fontSize: 20,
+                                        color: Hexcolor('#FFFFFF'))),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        )),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Card(
+                        color: Hexcolor('#B2EC80'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                            bottom: Radius.circular(10.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        child: Center(child: Text("hello")),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Card(
+                        color: Hexcolor('#B2EC80'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                            bottom: Radius.circular(10.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        child: Center(child: Text("hello")),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Card(
+                        color: Hexcolor('#B2EC80'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                            bottom: Radius.circular(10.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        child: Center(child: Text("hello")),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Card(
+                        color: Hexcolor('#B2EC80'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                            bottom: Radius.circular(10.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        child: Center(child: Text("hello")),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      width: 150,
+                      child: Card(
+                        color: Hexcolor('#B2EC80'),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(10.0),
+                            bottom: Radius.circular(10.0),
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 10.0),
+                        child: Center(child: Text("hello")),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
