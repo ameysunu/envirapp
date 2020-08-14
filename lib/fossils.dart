07be6aefce9454f4648084fa80ccefc79f7ee040
ameysunu <ameysunu00719@gmail.com>
2020-08-14 14:53:16 +0300

    fossils added

--- lib/fossils.dart
+++ lib/fossils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Fossils extends StatefulWidget {
   @override
@@ -12,89 +13,56 @@ class _FossilsState extends State<Fossils> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Hexcolor("#FF6B69"),
+        backgroundColor: Hexcolor("#984142"),
         body: SingleChildScrollView(
           child: SafeArea(
             child: Column(
               children: <Widget>[
-                Center(
-                  child: Container(
-                    child: Text(
-                      "Animals",
-                      style: TextStyle(
-                          color: Hexcolor("#FFFFFF"),
-                          fontFamily: 'OpenSans',
-                          fontSize: 35.0),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Card(
+                    color: Hexcolor('#6F790E'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(25.0),
+                        bottom: Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Center(
+                        child: Text(
+                          "Fossils",
+                          style: TextStyle(
+                            fontFamily: 'OpenSans',
+                            color: Hexcolor('#FFE6D7'),
+                            fontSize: 30,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-                Card(
-                  color: Hexcolor('#C35CAF'),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(25.0),
-                      bottom: Radius.circular(25.0),
-                    ),
-                  ),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                  child: SizedBox(
-                    height: 340.0,
-                    width: double.infinity,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                                child: SizedBox(
-                                  height: 100,
-                                  width: 100,
-                                  child: Image.asset("images/animals.png"),
-                                ),
-                              ),
-                              Text(
-                                "Helluuu, I'm a tiger! We are getting\nextinct with time. Save our families!",
-                                style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontSize: 15,
-                                  color: Colors.white70,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                            child: Text(
-                              'Over the last century, tiger habitats have fallen by around 95 percent, and there are now fewer than 4,000 tigers alive in the wild, reports the World Wildlife Federation (WWF). Of the nine subspecies of tiger, three are already extinct; and the rest are considered endangered.',
-                              style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontSize: 16,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                            child: Text(
-                              'Source: Reader\'s digest.',
-                              style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontStyle: FontStyle.italic,
-                                  fontSize: 13.0,
-                                  color: Colors.white70),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: InkWell(
+                    child: Image.asset("images/fossils.png"),
+                    onTap: () async {
+                      const url =
+                          'https://www.dep.pa.gov/Business/Land/Waste/Recycling/Benefits/Pages/NaturalResources.aspx';
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    "Animals are an important asset to our planet and with time, we are risking and killing them for human needs such as skin, leather and various other needs. From the jungle to the oceans, wild animals face ongoing threats of cruelty and abuse. Bears are captured and forced to fight dogs. Marine mammals, such as whales and dolphins, forced to perform in cruel entertainment venues. Wild animals are traded as exotic pets or unproven medicine.",
+                    "Coal, Petrol, Natural gases contribute to our fossils. Well, we don't have much of them left now. If we don't start conserving these, then we would move back to stone age. Various reduction methods like car pooling, switching off engines when not in use help the cause.",
                     style: TextStyle(
                       fontFamily: 'OpenSans',
                       color: Colors.white,
@@ -140,7 +108,7 @@ class _FossilsState extends State<Fossils> {
                     child: Padding(
                       padding: const EdgeInsets.all(20.0),
                       child: Text(
-                        "By supporting organizations like PETA, WWF and World Animal Protection, you are not just helping to change one life, but many more. Animals belong to their habitats just like humans belong to theirs. Let's not invade their space and also by donating, you are supporting the cause.",
+                        "To know about this, tap on the icon above to navigate and learn about how, we as humans can start saving up for future.",
                         style: TextStyle(
                           fontFamily: 'OpenSans',
                           fontSize: 15,
