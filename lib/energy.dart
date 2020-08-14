75f350e74cb7da07767eb3bc571ce671e74921c3
ameysunu <ameysunu00719@gmail.com>
2020-08-14 13:56:47 +0300

    energy added

--- lib/energy.dart
+++ lib/energy.dart
@@ -12,81 +12,32 @@ class _EnergyState extends State<Energy> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Hexcolor("#FF6B69"),
+        backgroundColor: Hexcolor("#FF774F"),
         body: SingleChildScrollView(
           child: SafeArea(
             child: Column(
               children: <Widget>[
-                Center(
-                  child: Container(
-                    child: Text(
-                      "Animals",
-                      style: TextStyle(
-                          color: Hexcolor("#FFFFFF"),
-                          fontFamily: 'OpenSans',
-                          fontSize: 35.0),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Card(
+                    color: Hexcolor('#2769C6'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(25.0),
+                        bottom: Radius.circular(25.0),
+                      ),
                     ),
-                  ),
-                ),
-                Card(
-                  color: Hexcolor('#C35CAF'),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(25.0),
-                      bottom: Radius.circular(25.0),
-                    ),
-                  ),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                  child: SizedBox(
-                    height: 340.0,
-                    width: double.infinity,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                                child: SizedBox(
-                                  height: 100,
-                                  width: 100,
-                                  child: Image.asset("images/animals.png"),
-                                ),
-                              ),
-                              Text(
-                                "Helluuu, I'm a tiger! We are getting\nextinct with time. Save our families!",
-                                style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontSize: 15,
-                                  color: Colors.white70,
-                                ),
-                              ),
-                            ],
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Center(
+                        child: Text(
+                          "Energy",
+                          style: TextStyle(
+                            fontFamily: 'OpenSans',
+                            color: Hexcolor('#FFE6D7'),
+                            fontSize: 30,
                           ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                            child: Text(
-                              'Over the last century, tiger habitats have fallen by around 95 percent, and there are now fewer than 4,000 tigers alive in the wild, reports the World Wildlife Federation (WWF). Of the nine subspecies of tiger, three are already extinct; and the rest are considered endangered.',
-                              style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontSize: 16,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                            child: Text(
-                              'Source: Reader\'s digest.',
-                              style: TextStyle(
-                                  fontFamily: 'OpenSans',
-                                  fontStyle: FontStyle.italic,
-                                  fontSize: 13.0,
-                                  color: Colors.white70),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
@@ -94,7 +45,7 @@ class _EnergyState extends State<Energy> {
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    "Animals are an important asset to our planet and with time, we are risking and killing them for human needs such as skin, leather and various other needs. From the jungle to the oceans, wild animals face ongoing threats of cruelty and abuse. Bears are captured and forced to fight dogs. Marine mammals, such as whales and dolphins, forced to perform in cruel entertainment venues. Wild animals are traded as exotic pets or unproven medicine.",
+                    "Energy is an important source of energy. You need energy to run mobile phones, laptops, home appliances and literally anything electrical.\nClick on the button for everytime, you use an electrical product and you'll see how important energy is",
                     style: TextStyle(
                       fontFamily: 'OpenSans',
                       color: Colors.white,
