0f3465f9949a38325bf4e9403eaa6aeaacf1d065
ameysunu <ameysunu00719@gmail.com>
2020-08-14 20:05:07 +0300

    UI fixed

--- lib/water.dart
+++ lib/water.dart
@@ -141,6 +141,29 @@ class _WaterState extends State<Water> {
                     ),
                   ],
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Card(
+                    color: Hexcolor('#FFC1B6'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(25.0),
+                        bottom: Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Hustle all what you want, but we all know how important water is, for all our basic needs from showering to drinking, we need water to survive. So why not save it? There is only 0.4% of fresh water available in the world. 0.4% for 7 billion people. Tap here to know more about water savings!",
+                        style: TextStyle(
+                          fontFamily: 'OpenSans',
+                          fontSize: 15,
+                          color: Hexcolor('#176BBF'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
